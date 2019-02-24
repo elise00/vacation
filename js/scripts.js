5d86e22109a4f5fce478478680b62a4721391170
eljc00 <elise.j.choi@gmail.com>
2019-02-24 00:45:35 -0800

    Add background image and play with opacity and fit. Adjust padding of jumbotron.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,25 @@
 $(document).ready(function() {
-    $("form").submit(function(event) {
-      //add vars for diff id tags
-      //var total = add up all answers
-      var question1 = parseInt($("select#environment").val());
-      var question2 = parseInt($("select#sell2").val());
-      var question3 = parseInt($("select#sell3").val());
-      var question4 = parseInt($("select#sell4").val());
-      var question5 = parseInt($("select#sell5").val());
+  $("form").submit(function(event) {
+      
+      //var total = add up answers
+      var question1 = parseInt($("input.environment").val());
+      var question3 = parseInt($("input.color").val());
+      var question2 = parseInt($("input.activity").val());
+      var question4 = parseInt($("input.alterego").val());
+      var question5 = parseInt($("input.future").val());
   
       var total = question1 + question2 + question3 + question4 + question5;
   
-      if (question1 === 0 || question2 === 0 || question3 === 0 || question4 === 0 || question5 === 0) {
-        alert("You haven't answered all the questions!")
-      }
-  
-      else if (total <= 6) {
+      if (total <= 6) {
         $(".dune").show();
-        $("body").addClass("dune-background");
+        $("body").addClass();
       }
       else if (total < 6 || total <= 12) {
-        $(".jurassic").show();
+        $(".tron").show();
         $("body").addClass("jurassic-background");
       }
       else if (total === 13 || total <= 18) {
-        $(".tron").show();
+        $(".jurassic").show();
         $("body").addClass("tron-background");
       }
 
