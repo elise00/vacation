071b308526e4d56ee965aa5fecd349d02c2ae6d1
eljc00 <elise.j.choi@gmail.com>
2019-02-23 16:01:54 -0800

    Add forms to HTML; add vars to JS.

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("form").submit(function(event) {
+      //add vars for diff id tags
+      //var total = add up all answers
+      var question1 = parseInt($("select#environment").val());
+      var question2 = parseInt($("select#sell2").val());
+      var question3 = parseInt($("select#sell3").val());
+      var question4 = parseInt($("select#sell4").val());
+      var question5 = parseInt($("select#sell5").val());
+  
+      var total = question1 + question2 + question3 + question4 + question5;
+  
+      if (question1 === 0 || question2 === 0 || question3 === 0 || question4 === 0 || question5 === 0) {
+        alert("You haven't answered all the questions!")
+      }
+  
+      else if (total <= 6) {
+        $(".amazon").show();
+        $("body").addClass("amazon-background");
+      }
+      else if (total < 6 || total <= 12) {
+        $(".nepal").show();
+        $("body").addClass("nepal-background");
+      }
+      else if (total === 13 || total <= 18) {
+        $(".oregon").show();
+        $("body").addClass("oregon-background");
+      }
+      else if (total === 19 || total <= 24) {
+        $(".jamaica").show();
+        $("body").addClass("jamaica-background");
+      }
+      event.preventDefault();
+  
+    });
+  });
