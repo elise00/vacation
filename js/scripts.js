79a948b5d32c91a849d7eabed58a6ea4f5034cde
eljc00 <elise.j.choi@gmail.com>
2019-02-25 22:59:25 -0800

    Push laptop changes (after class)

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,25 @@
-// $(document).ready(function() {
-//   $("form").submit(function(event) {
-      
-//       //var total = add up answers
-//       var question1 = parseInt($("input.environment").val());
-//       var question2 = parseInt($("input.color").val());
-//       var question3 = parseInt($("input.activity").val());
-//       var question4 = parseInt($("input.alterego").val());
-//       var question5 = parseInt($("input.future").val());
-  
-//       var total = question1 + question2 + question3 + question4 + question5;
-  
-//       if (total <= 6) {
-//         $(".dune").show();
-//         $("body").addClass();
-//       }
-//       else if (total < 6 || total <= 12) {
-//         $(".tron").show();
-//         $("body").addClass("jurassic-background");
-//       }
-//       else if (total === 13 || total <= 18) {
-//         $(".jurassic").show();
-//         $("body").addClass("tron-background");
-//       }
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    
+    var question1 = parseInt($("environmentChoice1").val());
+    var question2 = parseInt($("question2").val());
+    var question3 = parseInt($("question3").val());
+    var question4 = parseInt($("question4").val());
+    var question5 = parseInt($("question5").val());
 
-//       event.preventDefault();
-  
-//     });
-//   });
+    // var total = $("input:radio[name=environment]:checked").val();
+    var total = environmentChoice1 + question2 + question3 + question4 + question5;
+
+      if (total <= 6) {
+        $(".dune").show();
+      } else if (total < 6 || total <= 12) {
+        $(".jurassic").show();
+      } else if (total === 13 || total <= 18) {
+        $(".tron").show();
+      }
+      event.preventDefault();
+    });
+  });
 
 // $(document).ready(function() {
 //   $("#formz").submit(function(event) {
@@ -80,3 +73,4 @@
 //     event.preventDefault();
 //   });
 // });
+
