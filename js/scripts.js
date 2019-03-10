8574b2b788aeb4ff1774f3c448ee5a107487656f
eljc00 <elise.j.choi@gmail.com>
2019-03-09 17:28:18 -0800

    Add parseInt to JS radio vars.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,17 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
 
-    // var question2 = parseInt($("question2").val());
-    // var question2 = parseInt($("question2").val());
-    // var question3 = parseInt($("question3").val());
-    // var question4 = parseInt($("question4").val());
-    // var question5 = parseInt($("question5").val());
-
-    var question1 = $("input:radio[name=environment]:checked").val();
-    var question2 = $("input:radio[name=color]:checked").val();
-    var question3 = $("input:radio[name=activity]:checked").val();
-    var question4 = $("input:radio[name=alterego]:checked").val();
-    var question5 = $("input:radio[name=future]:checked").val();
+    var question1 = parseInt($("input:radio[name=environment]:checked").val());
+    var question2 = parseInt($("input:radio[name=color]:checked").val());
+    var question3 = parseInt($("input:radio[name=activity]:checked").val());
+    var question4 = parseInt($("input:radio[name=alterego]:checked").val());
+    var question5 = parseInt($("input:radio[name=future]:checked").val());
 
     var total = question1 + question2 + question3 + question4 + question5;
 
       if (total <= 3) {
         $(".dune").show();
-      } else if (total <= 6) {
+      } else if (total <= 5) {
         $(".jurassic").show();
       } else {
         $(".tron").show();
